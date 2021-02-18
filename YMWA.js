5febf0de1f4dedeab7d0da5ba0f790e2cb6d0819
JesusMuentes <75540712+JesusMuentes@users.noreply.github.com>
2021-02-18 16:13:13 -0500

    Update YMWA.js

--- YMWA.js
+++ YMWA.js
@@ -3,10 +3,6 @@
 	"download": "https://ymmods0.blogspot.com/2021/01/YMWhatsApp.html",
 	"update": [
 		{
-			"languaje": "es",
-			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-es.html"
-		},
-		{
 			"languaje": "en",
 			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-en.html"
 		}

172e17830f96c7833c75ee1d9627f19533389781
JesusMuentes <75540712+JesusMuentes@users.noreply.github.com>
2021-03-03 14:36:45 -0500

    Add files via upload

---
+++ YMWAB.js
@@ -0,0 +1,14 @@
+{
+	"version": "5.0",
+	"download": "https://ymmods0.blogspot.com/2021/01/YMWhatsAppBusiness.html",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-en.html"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-es.html"
+		}
+	]
+}

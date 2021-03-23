f6d3d6ef0608dc1c104ba07d8354f729a4b6744c
JesusMuentes <75540712+JesusMuentes@users.noreply.github.com>
2021-03-23 16:41:56 -0500

    Add files via upload

---
+++ prueba1.js
@@ -0,0 +1,10 @@
+{
+	"version": "13.0",
+	"download": "https://ymmods0.blogspot.com/2021/01/YMWhatsApp.html",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-en.html"
+		}
+	]
+}

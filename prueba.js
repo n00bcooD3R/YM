aa54d03b5fa4a1701f4c47bf1e15c3ecf94df3d7
JesusMuentes <75540712+JesusMuentes@users.noreply.github.com>
2021-05-02 17:29:04 -0500

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,18 @@
+{
+	"version": "16.0",
+	"download": "https://ymmods-official.blogspot.com/2021/04/ymwa.html",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-en.html"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-es.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/JesusMuentes/YM/main/changelog-pt.html"
+		}
+	]
+}
